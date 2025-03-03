050ae07a586554c25988b4ab2259b16aaab26a20
Kristyna Sedlackova <sedlackova.kri@gmail.com>
2025-03-03 21:17:01 +0100

    Vyreseny ukol02 - banka na kontrolu

--- 02/banka/banka.py
+++ 02/banka/banka.py
@@ -23,3 +23,53 @@
 #
 # Pro tento pokročilý způsob je však třeba použít pokročilou knihovnu pro práci
 # s parametry příkazové řádky, jako např. argparse nebo click.
+
+
+import argparse 
+
+def get_balance(account_number):
+    try:
+        file = open(str(account_number))
+        balance = file.read()
+        file.close() 
+        return int(balance)
+    except FileNotFoundError:
+        return f"Chyba: Účet s číslem {account_number} neexistuje."
+
+def update_balance(account_number, amount): 
+    file = open(str(account_number), "w")
+    file.write(str(amount))
+
+def transfer_amount(from_account_number, to_account_number, amount):
+    amount = int(amount)
+    new_balance_from = get_balance(from_account_number)
+    new_balance_to = get_balance(to_account_number)
+
+    if amount <= 0:
+        raise ValueError("Lze zadat pouze zadat kladnou částku.")
+
+    if new_balance_from < amount: 
+        raise ValueError("Na účtě není dostatek peněz.")
+    
+    if from_account_number == to_account_number:
+        raise ValueError("Účet příjemce a odesílatele nesmí být stejný.")
+        
+    new_balance_from -= amount 
+    update_balance(from_account_number, new_balance_from)
+    new_balance_to += amount
+    update_balance(to_account_number, new_balance_to)  
+
+
+parser = argparse.ArgumentParser() 
+parser.add_argument("--from", type=int, required=True, dest="from_account")
+parser.add_argument("--to", type=int, required=True, dest="to_account")
+parser.add_argument("--amount", type=int, required=True, dest="amount")
+ 
+arguments = parser.parse_args() 
+
+from_account_number = arguments.from_account
+to_account_number = arguments.to_account
+amount = arguments.amount  
+
+transfer_amount(from_account_number, to_account_number, amount)
+print(f"Úspěšně převedeno {amount} z účtu {from_account_number} na účet {to_account_number}.")
